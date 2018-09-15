eeba80490d989a2701b87305112c74cb8f7f797f
sully34 <sssulai@gmail.com>
2018-09-15 18:19:19 -0400

    updated the apiRoutes file and others

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,3 +1,20 @@
-app.get('/api/function,function(req,res){
-    res.send('')
-}
+//app.get('/api/function,function(req,res){
+//    res.send('')
+//}
+
+//var express = require('express')
+//var app = express()
+
+//app.get('/survey,function(req,res){
+//    res.send('')
+//}
+
+// GET method route
+app.get('/api'/friends, function (req, res) {
+    res.send('GET request to the survey')
+  })
+  
+  // POST method route
+  app.post('/api'/friends, function (req, res) {
+    res.send('POST request to the homepage')
+  })
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,6 +1,16 @@
-var express = require('express')
-var app = express()
+//var express = require('express')
+//var app = express()
 
-app.get('/survey,function(req,res){
-    res.send('')
-}
+//app.get('/survey,function(req,res){
+//    res.send('')
+//}
+
+// GET method route
+app.get('/survey', function (req, res) {
+    res.send('GET request to the survey')
+  })
+  
+  // POST method route
+  app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
